199b45b9a6a114034b9d3643b4b6d08eee51a86a
Willy <jemand771@gmx.net>
2022-01-25 14:38:29 +0100

    modify event key to not use activity id
    nextcloud rassigns these IDs seemingly at random, causing events to be reported multiple times
    (once for every id it ever gets)
    
    since the combination of create/delete and a file id *should* always be unique, we can rely on it as a unique identifier instead.
    
    for file_changed events, fall back to the old key style for now

--- model.py
+++ model.py
@@ -18,7 +18,7 @@ class EventData:
 
     @property
     def key(self):
-        return f"{self.parent_activity_id}_{self.file_id}"
+        return f"{self.action}/{self.file_id}" + f"/{self.parent_activity_id}" if self.action == "file_changed" else ""
 
     @property
     def file_name(self):
